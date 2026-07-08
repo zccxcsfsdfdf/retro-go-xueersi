71aa1df5eaf1265ced2c7b36658366d117d953ab
Jack <820224533@qq.com>
2026-07-08 15:01:39 +0800

    Add files via upload

---
+++ retro-core/main/main.c
@@ -0,0 +1,37 @@
+#include "shared.h"
+
+extern void vTaskPrioritySet(void *task, unsigned int priority);
+
+void app_main(void)
+{
+    rg_app_t *app = rg_system_init(AUDIO_SAMPLE_RATE, NULL, NULL);
+
+    RG_LOGI("configNs=%s", app->configNs);
+
+    vTaskPrioritySet(NULL, 6);
+
+    if (strcmp(app->configNs, "gbc") == 0 || strcmp(app->configNs, "gb") == 0)
+        gbc_main();
+    else if (strcmp(app->configNs, "nes") == 0)
+        nes_main();
+    else if (strcmp(app->configNs, "pce") == 0)
+        pce_main();
+    else if (strcmp(app->configNs, "sms") == 0)
+        sms_main();
+    else if (strcmp(app->configNs, "gg") == 0)
+        sms_main();
+    else if (strcmp(app->configNs, "col") == 0)
+        sms_main();
+    else if (strcmp(app->configNs, "gw") == 0)
+        gw_main();
+    else if (strcmp(app->configNs, "snes") == 0)
+        snes_main();
+#ifndef __TINYC__
+    else if (strcmp(app->configNs, "lnx") == 0)
+        lynx_main();
+#endif
+    else
+        launcher_main();
+
+    RG_PANIC("Never reached");
+}
